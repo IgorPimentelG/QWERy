19e0bf54d9090f45f42cce6b86ba0035501e4171
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-06-14 00:59:23 -0300

    🍱 styles: add fonts, imagens and default styles

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as CoverImage } from './images/cover.png';
+export { default as MountainSmallImage } from './images/mountain-1.png';
+export { default as MountainLargeImage } from './images/mountain-2.png';
+export { default as PeopleImage } from './images/people.png';
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+import '@global/styles/colors.css';
+import '@global/styles/default.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
